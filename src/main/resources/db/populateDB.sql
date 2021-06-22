2df34e8185ba3f4fe55e933afc6f51f2b2854113
Dmytro Ivakhnenko <dimsaid2dim@gmail.com>
2021-06-22 20:49:01 +0300

    HW03 Add create/init scripts for meal, add implementation for JdbcMealRepository

--- src/main/resources/db/populateDB.sql
+++ src/main/resources/db/populateDB.sql
@@ -1,3 +1,4 @@
+DELETE FROM meals;
 DELETE FROM user_roles;
 DELETE FROM users;
 ALTER SEQUENCE global_seq RESTART WITH 100000;
@@ -9,3 +10,16 @@ VALUES ('User', 'user@yandex.ru', 'password'),
 INSERT INTO user_roles (role, user_id)
 VALUES ('USER', 100000),
        ('ADMIN', 100001);
+
+INSERT INTO meals (description, datetime, calories, user_id)
+VALUES ('Breakfast', '2020-01-30 10:00:00', 500, 100000),
+       ('Lunch', '2020-01-30 13:00:00', 1000, 100000),
+       ('Supper', '2020-01-30 20:00:00', 500, 100000),
+       ('Edge meal value', '2020-01-31 00:00:00', 100, 100000),
+       ('Breakfast', '2020-01-31 10:00:00', 1000, 100000),
+       ('Lunch', '2020-01-31 13:00:00', 500, 100000),
+       ('Supper', '2020-01-31 20:00:00', 410, 100000),
+       ('Admin Breakfast', '2020-01-30 10:00:00', 100, 100001),
+       ('Admin Lunch', '2020-01-30 13:00:00', 2000, 100001),
+       ('Admin Supper', '2020-01-30 20:00:00', 500, 100001);
+
